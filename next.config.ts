1e9ffbb712d8c24d7842b4e3b7d952aa55713afb
Jonathan Kennell <jonathan.kennell@gmail.com>
2025-02-06 11:33:44 -0500

    Refine GitHub Pages deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next"
 //const isProd = process.env.NODE_ENV === "production"
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  // assetPrefix: isProd ? "/jonathankennell.com/" : "",
-  // basePath: isProd ? "/jonathankennell.com" : "",
   output: "export",
 }
 
