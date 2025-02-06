a34c0de9ed6e97de33e2b84d5d6b1c303d83d3f0
Jonathan Kennell <jonathan.kennell@gmail.com>
2025-02-06 09:26:52 -0500

    Configure homepage and remove Next.js image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "export", // Enables static HTML export
-  images: { unoptimized: true }, // Needed if using Next.js image optimization
 }
 
 export default nextConfig
